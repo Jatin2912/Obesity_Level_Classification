e21b35629d9e5b793881df777580134bcd9436b6
Jatin2912 <jatinverma2912@gmail.com>
2024-01-21 00:19:07 +0530

    Data Ingestion Completed

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,59 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+from src.utils import read_sql_data
+
+from sklearn.model_selection import train_test_split
+
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path:str=os.path.join('artifacts','train.csv')
+    test_data_path:str=os.path.join('artifacts','test.csv')
+    raw_data_path:str=os.path.join('artifacts','raw.csv')
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info('Data Ingestion method starts')
+
+        try:
+            ##reading the data from mysql
+            df=read_sql_data()
+            logging.info("Reading completed mysql database")
+
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+
+            logging.info("Train test split")
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+            logging.info("Data Ingestion is completed")
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+
+            )
+
+        except Exception as e:
+            raise CustomException(e,sys)
+
+##### The below code is only for checking and not required
+if __name__=="__main__":
+    logging.info("Logging has started")
+    try:
+        data_ingestion=DataIngestion()
+        data_ingestion.initiate_data_ingestion()
+    except Exception as e:
+        logging.info('Division by zero') 
+        raise CustomException(e,sys)
