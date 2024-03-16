f6783f886552edf7c751a18abfd0add071152b94
Jatin2912 <jatinverma2912@gmail.com>
2024-03-16 23:24:52 +0530

    Deployment Configuration

--- application.py
+++ application.py
@@ -59,5 +59,5 @@ def classify_datapoint():
         return render_template('form.html', results=class_label)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",debug=True)
+    app.run(host="0.0.0.0")
 
